48553c31fecb8f169aa624f6699c2506d0c6cc4c
Wolfgang Popp <mail@wolfgang-popp.de>
2016-08-11 09:31:18 +0200

    completions: added --no-header and --no-video flags

--- completions/fish/ytcc.fish
+++ completions/fish/ytcc.fish
@@ -34,6 +34,8 @@ complete -c $cmd -f -r -l to              -s t -d "apply date end filter"
 complete -c $cmd    -r -l path            -s p -d "set the download path"
 complete -c $cmd -f    -l no-description  -s g -d "do not print the video description"
 complete -c $cmd -f -r -l columns         -s o -d "column" -a 'ID Date Channel Title URL'
+complete -c $cmd -f    -l no-header       -s   -d "don't print table header"
+complete -c $cmd -f    -l no-video        -s x -d "audio only"
 complete -c $cmd -f    -l yes             -s y -d "automatically answer all questions with yes"
 complete -c $cmd    -r -l import-from     -s y -d "import subscriptions from youtube"
 complete -c $cmd -x    -l cleanup              -d "cleanup and shrink database file"
