271dbb8030dd61e86cda7ff689281b2c9a0ebeae
Wolfgang Popp <mail@wolfgang-popp.de>
2016-08-14 15:47:40 +0200

    released version 1.5.0

--- ytcc/__init__.py
+++ ytcc/__init__.py
@@ -23,6 +23,6 @@ signing up for a Google account.
 """
 
 __license__ = "GPLv3"
-__version__ = "1.4.0"
+__version__ = "1.5.0"
 __author__ = __maintainer__ = "Wolfgang Popp"
 __email__ = "mail@wolfgang-popp.de"
