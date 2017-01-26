603f785fe950bfe09658564e1380599deb8d0381
Wolfgang Popp <mail@wolfgang-popp.de>
2017-01-26 10:33:41 +0100

    marked more strings for translation

--- ytcc/cli.py
+++ ytcc/cli.py
@@ -29,7 +29,7 @@ from dateutil import parser as date_parser
 ytcc_core = core.Ytcc()
 interactive_enabled = True
 description_enabled = True
-table_header = ["ID", "Date", "Channel", "Title", "URL"]
+table_header = [_("ID"), _("Date"), _("Channel"), _("Title"), _("URL")]
 column_filter = None
 header_enabled = True
 no_video = False
@@ -132,8 +132,9 @@ def play(video, audio_only):
     maybe_print_description(video.description)
     if not ytcc_core.play_video(video.id, audio_only):
         print()
-        print(_("WARNING: The video player terminated with an error."))
-        print(_("         The last video is not marked as watched!\n"))
+        print(_("WARNING: The video player terminated with an error.\n"
+                "         The last video is not marked as watched!"))
+        print()
 
 
 def watch(video_ids=None):
@@ -286,137 +287,138 @@ def download(video_ids, audio_only=None):
             ytcc_core.download_videos(
                 video_ids=video_ids, path=download_path, no_video=not audio_only)
     except core.DownloadError as e:
-        print(_("The video has not been downloaded due to the following error."))
+        print(_("The video has not been downloaded due to the following error:"))
         print(e)
         print()
 
 
 def parse_args():
-    parser = argparse.ArgumentParser(description="ytcc is a commandline YouTube client that keeps "
-                                     "track of your favorite channels. The --list, --watch, "
-                                     "--download, --mark-watched options can be combined with "
-                                     "filter options --channel-filter, --include-watched, --since,"
-                                     " --to")
+    parser = argparse.ArgumentParser(
+        description=_("ytcc is a commandline YouTube client that keeps track of your favorite "
+                      "channels. The --list, --watch, --download, --mark-watched options can be "
+                      "combined with filter options --channel-filter, --include-watched, --since,"
+                      " --to"))
 
     parser.add_argument("-a", "--add-channel",
-                        help="add a new channel. NAME is the name displayed by ytcc. URL is the "
-                        "url of the channel's front page",
+                        help=_("add a new channel. NAME is the name displayed by ytcc. URL is the "
+                        "url of the channel's front page"),
                         nargs=2,
                         metavar=("NAME", "URL"))
 
     parser.add_argument("-c", "--list-channels",
-                        help="print a list of all subscribed channels",
+                        help=_("print a list of all subscribed channels"),
                         action="store_true")
 
     parser.add_argument("-r", "--delete-channel",
-                        help="unsubscribe from the channel identified by 'ID'",
+                        help=_("unsubscribe from the channel identified by 'ID'"),
                         metavar="ID",
                         nargs='+',
                         type=str)
 
     parser.add_argument("-u", "--update",
-                        help="update the video list",
+                        help=_("update the video list"),
                         action="store_true")
 
     parser.add_argument("-l", "--list",
-                        help="print a list of videos that match the criteria given by the filter "
+                        help=_("print a list of videos that match the criteria given by the "
+                            "filter")
                         "options",
                         action="store_true")
 
     parser.add_argument("-w", "--watch",
-                        help="play the videos identified by 'ID'. Omitting the ID will play all "
-                        "videos specified by the filter options",
+                        help=_("play the videos identified by 'ID'. Omitting the ID will play all "
+                        "videos specified by the filter options"),
                         nargs='*',
                         type=int,
                         metavar="ID")
 
     parser.add_argument("-d", "--download",
-                        help="download the videos identified by 'ID'. The videos are saved "
+                        help=_("download the videos identified by 'ID'. The videos are saved "
                         "in $HOME/Downloads by default. Omitting the ID will download "
-                        "all videos that match the criteria given by the filter options",
+                        "all videos that match the criteria given by the filter options"),
                         nargs="*",
                         type=int,
                         metavar="ID")
 
     parser.add_argument("-m", "--mark-watched",
-                        help="mark videos identified by ID as watched. Omitting the ID will mark "
-                        "all videos that match the criteria given by the filter options as "
-                        "watched",
+                        help=_("mark videos identified by ID as watched. Omitting the ID will mark"
+                        " all videos that match the criteria given by the filter options as "
+                        "watched"),
                         nargs='*',
                         type=int,
                         metavar="ID")
 
     parser.add_argument("-f", "--channel-filter",
-                        help="plays, lists, marks, downloads only videos from channels defined in "
-                        "the filter",
+                        help=_("plays, lists, marks, downloads only videos from channels defined "
+                        "in the filter"),
                         nargs='+',
                         type=str,
                         metavar="NAME")
 
     parser.add_argument("-n", "--include-watched",
-                        help="include already watched videos to filter rules",
+                        help=_("include already watched videos to filter rules"),
                         action="store_true")
 
     parser.add_argument("-s", "--since",
-                        help="includes only videos published after the given date",
+                        help=_("includes only videos published after the given date"),
                         metavar="YYYY-MM-DD",
                         type=is_date)
 
     parser.add_argument("-t", "--to",
-                        help="includes only videos published before the given date",
+                        help=_("includes only videos published before the given date"),
                         metavar="YYYY-MM-DD",
                         type=is_date)
 
     parser.add_argument("-q", "--search",
-                        help="searches for the given PATTERN. The pattern can specify one of the "
-                        "three columns 'channel', 'title', 'description'. If no column is "
+                        help=_("searches for the given PATTERN. The pattern can specify one of the"
+                        " three columns 'channel', 'title', 'description'. If no column is "
                         "specified, all columns are searched. The pattern can also specify '*' "
                         "wildcards. Example: --search 'title:box*' will find all video that have "
                         "a word that starts with 'box' in their title. If this flag is enabled, "
-                        "the -f, -n, " "-s, -t flags will be ignored.",
+                        "the -f, -n, " "-s, -t flags will be ignored."),
                         metavar="PATTERN")
 
     parser.add_argument("-p", "--path",
-                        help="set the download path to PATH",
+                        help=_("set the download path to PATH"),
                         metavar="PATH",
                         type=is_directory)
 
     parser.add_argument("-g", "--no-description",
-                        help="do not print the video description before playing the video",
+                        help=_("do not print the video description before playing the video"),
                         action="store_true")
 
     parser.add_argument("-o", "--columns",
-                        help="specifies which columns will be printed when listing videos. COL "
+                        help=_("specifies which columns will be printed when listing videos. COL ")
                         "can be any of " + str(table_header),
                         nargs='+',
                         metavar="COL",
                         choices=table_header)
 
     parser.add_argument("--no-header",
-                        help="do not print the header of the table when listing videos",
+                        help=_("do not print the header of the table when listing videos"),
                         action="store_true")
 
     parser.add_argument("-x", "--no-video",
-                        help="plays or downloads only the audio part of a video",
+                        help=_("plays or downloads only the audio part of a video"),
                         action="store_true")
 
     parser.add_argument("-y", "--yes",
-                        help="automatically answer all questions with yes",
+                        help=_("automatically answer all questions with yes"),
                         action="store_true")
 
     parser.add_argument("--import-from",
-                        help="import YouTube channels from YouTube's subscription export "
-                        "(available at https://www.youtube.com/subscription_manager)",
+                        help=_("import YouTube channels from YouTube's subscription export "
+                        "(available at https://www.youtube.com/subscription_manager)"),
                         metavar="PATH",
                         type=argparse.FileType("r"))
 
     parser.add_argument("--cleanup",
-                        help="removes old videos from the database and shrinks the size of the "
-                        "database file",
+                        help=_("removes old videos from the database and shrinks the size of the "
+                        "database file"),
                         action="store_true")
 
     parser.add_argument("-v", "--version",
-                        help="output version information and exit",
+                        help=_("output version information and exit"),
                         action="store_true")
 
     args = parser.parse_args()
