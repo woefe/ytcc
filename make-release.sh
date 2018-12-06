fb140558db07f9bc1a4a3f7e568179e8b6d7aa1c
Wolfgang Popp <mail@wolfgang-popp.de>
2018-12-06 20:16:16 +0100

    Update release script

--- make-release.sh
+++ make-release.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 
-if [[ -z $1 || $1 == "-h" ]]; then
+tagname=$1
+
+if ! echo "$tagname" | grep -Eo '[0-9]+\.[0-9]+\.[0-9]+'; then
     echo "Usage: $0 major.minor.patch"
     exit 1
 fi
 
-tagname=$1
 sed -i -e "s/^__version__ = .*$/__version__ = \"$tagname\"/" ytcc/__init__.py
+git commit ytcc/__init__.py -m "Release version $tagname"
+git tag -a "v$tagname" -m "Version $tagname"
 
-git add ytcc/__init__.py
-git commit -m "released version $tagname"
+read -rp "Push changes? Ctrl+c to cancel, Enter to push"
 git push origin master
-
-git tag -a v$tagname -m "Version $tagname"
-git push origin v$tagname
+git push origin "v$tagname"
