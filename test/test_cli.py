ec1f4d104907c2c597b975e359f5bbc4285604b5
Wolfgang Popp <mail@wolfgang-popp.de>
2020-10-17 23:04:38 +0200

    Add more cli tests

--- test/test_cli.py
+++ test/test_cli.py
@@ -15,22 +15,114 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with ytcc.  If not, see <http://www.gnu.org/licenses/>.
+import contextlib
+from tempfile import NamedTemporaryFile
+from typing import Callable
 
 import pytest
-from click.testing import CliRunner
+from click.testing import CliRunner, Result
+
 from ytcc.cli import cli
 
 
 @pytest.fixture
-def cli_runner() -> CliRunner:
-    return CliRunner()
+def cli_runner(monkeypatch) -> Callable[..., Result]:
+    @contextlib.contextmanager
+    def context():
+        with NamedTemporaryFile() as db_file, NamedTemporaryFile("w") as conf_file:
+            conf_file.write("[ytcc]\n")
+            conf_file.write(f"db_path={db_file.name}\n")
+            conf_file.write("download_dir=/tmp/ytcc\n")
+            conf_file.flush()
+            yield lambda *args: CliRunner().invoke(cli, ["--conf", conf_file.name, *args])
+
+    return context
 
 
-def test_bug_report_command(cli_runner: CliRunner):
-    result = cli_runner.invoke(cli, "bug-report")
-    assert result.exit_code == 0
+def test_bug_report_command(cli_runner):
+    from ytcc import __version__
+    with cli_runner() as runner:
+        result = runner("bug-report")
+        assert result.exit_code == 0
+        assert result.stdout.startswith(f"---ytcc version---\n{__version__}")
+        assert "download_dir = /tmp/ytcc" in result.stdout
 
 
-def test_xsv_printer_option(cli_runner: CliRunner):
-    result = cli_runner.invoke(cli, ["--output", "xsv", "--separator", "ab"])
-    assert result.exit_code != 0
+def test_subscribe(cli_runner):
+    with cli_runner() as runner:
+        result = runner("subscribe", "WebDriver",
+                        "https://www.youtube.com/channel/UCsLiV4WJfkTEHH0b9PmRklw/videos")
+        assert result.exit_code == 0
+        result = runner("--output", "xsv", "subscriptions")
+        assert "WebDriver" in result.stdout
+
+
+def test_subscribe_duplicate(cli_runner):
+    with cli_runner() as runner:
+        result = runner("subscribe", "WebDriver",
+                        "https://www.youtube.com/channel/UCsLiV4WJfkTEHH0b9PmRklw/videos")
+        assert result.exit_code == 0
+        result = runner("subscribe", "WebDriver",
+                        "https://www.youtube.com/channel/UCsLiV4WJfkTEHH0b9PmRklw/videos")
+        assert result.exit_code != 0
+
+
+def test_unsubscribe(cli_runner):
+    with cli_runner() as runner:
+        result = runner("subscribe", "WebDriver",
+                        "https://www.youtube.com/channel/UCsLiV4WJfkTEHH0b9PmRklw/videos")
+        assert result.exit_code == 0
+
+        result = runner("unsubscribe", "--yes", "WebDriver")
+        assert result.exit_code == 0
+
+        result = runner("unsubscribe", "--yes", "WebDriver")
+        assert result.exit_code != 0
+
+
+def test_rename(cli_runner):
+    with cli_runner() as runner:
+        result = runner("subscribe", "WebDriver",
+                        "https://www.youtube.com/channel/UCsLiV4WJfkTEHH0b9PmRklw/videos")
+        assert result.exit_code == 0
+
+        result = runner("rename", "WebDriver", "WebDriverTorso")
+        assert result.exit_code == 0
+
+        result = runner("-o", "xsv", "subscriptions")
+        assert "WebDriverTorso" in result.stdout
+
+        result = runner("rename", "WebDriver", "WebDriverTorso")
+        assert result.exit_code != 0
+
+
+def test_update(cli_runner):
+    with cli_runner() as runner:
+        result = runner("subscribe", "WebDriver",
+                        "https://www.youtube.com/channel/UCsLiV4WJfkTEHH0b9PmRklw/videos")
+        assert result.exit_code == 0
+
+        result = runner("update", "--max-backlog", "20")
+        assert result.exit_code == 0
+
+        result = runner("--output", "xsv", "list")
+        assert len(result.stdout.splitlines()) == 20
+
+
+def test_download(cli_runner):
+    with cli_runner() as runner:
+        result = runner("subscribe", "WebDriver",
+                        "https://www.youtube.com/channel/UCsLiV4WJfkTEHH0b9PmRklw/videos")
+        assert result.exit_code == 0
+
+        result = runner("update", "--max-backlog", "20")
+        assert result.exit_code == 0
+
+        result = runner("download", "1")
+        assert result.exit_code == 0
+
+
+def test_xsv_printer_option(cli_runner):
+    with cli_runner() as runner:
+        result = runner("--output", "xsv", "--separator", "ab")
+        assert result.exit_code != 0
