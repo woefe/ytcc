9a44e1a4c9edc512cbf933d68531b590273729b8
Wolfgang Popp <mail@wolfgang-popp.de>
2020-10-17 15:45:17 +0200

    Add manpage generator to release script

--- scripts/make_release.sh
+++ scripts/make_release.sh
@@ -8,6 +8,8 @@ if ! echo "$tagname" | grep -Eo '[0-9]+\.[0-9]+\.[0-9]+'; then
 fi
 
 sed -i -e "s/^__version__ = .*$/__version__ = \"$tagname\"/" ytcc/__init__.py
+python3 scripts/make_doc.py doc/ytcc.1
+
 git commit ytcc/__init__.py -m "Release version $tagname"
 git tag -a "v$tagname" -m "Version $tagname"
 
